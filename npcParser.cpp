afe372810fed1659ddf1ee203209e8e920c4765a
Shane P Drafahl <sdrafahl@iastate.edu>
2016-10-30 23:14:11 -0500

    added additional code to parser

--- npcParser.cpp
+++ npcParser.cpp
@@ -10,14 +10,14 @@ void readFile(){
     while(s >> buffer){
         if(!buffer.compare("BEGIN")){
             if(!createMon){
-                string name;
-                string symb;
-                string color;
-                string desc;
-                string speed;
-                string dam;
-                string hp;
-                string abil;
+                string name =" ";
+                string symb = " ";
+                string color = " ";
+                string desc = " ";
+                string speed = " ";
+                string dam = " ";
+                string hp = " ";
+                string abil =" ";
                 s >> buffer;
                 if(!buffer.compare("MONSTER"){
                     s >> buffer;
@@ -25,32 +25,81 @@ void readFile(){
                         while(1){
                             s >> buffer;
                         if(validSyntax(buffer)){
-                            name=buffer;
+                            name+=buffer;
                         }else{
                             break;
                         }
                     }
                     }
                     if(!buffer.compare("SYMB")){
-
+                        while(1){
+                            s >> buffer;
+                        if(validSyntax(buffer)){
+                            symb+=buffer;
+                        }else{
+                            break;
+                        }
+                        }
                     }
                     if(!buffer.compare("COLOR")){
-
+                        while(1){
+                            s >> buffer;
+                        if(validSyntax(buffer)){
+                            color+=buffer;
+                        }else{
+                            break;
+                        }
+                        }
                     }
                     if(!buffer.compare("DESC")){
-
+                        while(1){
+                            s >> buffer;
+                        if(validSyntax(buffer)){
+                            desc+=buffer;
+                        }else{
+                            break;
+                        }
+                        }
                     }
                     if(!buffer.compare("SPEED")){
-
+                        while(1){
+                            s >> buffer;
+                        if(validSyntax(buffer)){
+                            speed+=buffer;
+                        }else{
+                            break;
+                        }
+                        }
                     }
                     if(!buffer.compare("DAM")){
-
+                        while(1){
+                            s >> buffer;
+                        if(validSyntax(buffer)){
+                            dam+=buffer;
+                        }else{
+                            break;
+                        }
+                        }
                     }
                     if(!buffer.compare("HP")){
-
+                        while(1){
+                            s >> buffer;
+                        if(validSyntax(buffer)){
+                            hp+=buffer;
+                        }else{
+                            break;
+                        }
+                        }
                     }
                     if(!buffer.compare("ABIL")){
-
+                        while(1){
+                            s >> buffer;
+                        if(validSyntax(buffer)){
+                            abil+=buffer;
+                        }else{
+                            break;
+                        }
+                        }
                     }
                     if(!buffer.compare("END")){
                         createMon=0;
